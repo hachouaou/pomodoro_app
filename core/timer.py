93217bd46aabf6c3009e7abc26076fe4b9194634
hachouaou <hachouoau@gmail.com>
2024-12-23 12:24:33 +0100

    modification timer.py

--- core/timer.py
+++ core/timer.py
@@ -1,18 +1,23 @@
 import time
 
 class PomodoroTimer:
-    def __init__(self, work_duration=50*60, break_duration=10*60):
+    def __init__(self, work_duration):
         self.work_duration = work_duration
-        self.break_duration = break_duration
-        self.current_time = work_duration
+        self.time_left = work_duration
         self.running = False
 
-    def start(self):
+    def format_time(self):
+        minutes = self.time_left // 60
+        seconds = self.time_left % 60
+        return f"{minutes:02}:{seconds:02}"
+    
+    def start(self, update_callback):
         self.running = True
-        while self.running and self.current_time > 0:
-            print(f'Time left: {self.current_time // 60}:{self.current_time % 60}')
+        while self.running and self.time_left > 0:
             time.sleep(1)
-            self.current_time -= 1
+            self.time_left -= 1
+            update_callback()
+        self.running = False
 
     def pause(self):
         self.running = False
